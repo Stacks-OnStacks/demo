c4c6b881b4b31ea5082acacaa9f108139a7359ea
JesterCharles <JesterCharles@gmail.com>
2022-07-26 17:55:56 -0400

    added the old school java program from terminal

---
+++ java/old_school/Hello.java
@@ -0,0 +1,28 @@
+// This is a comment
+
+// The keyword class defines a class, filename must EXACTLY match the classname
+// CLASSES ALWAYS START WITH A CAPITAL! Pascal Casing
+class Hello{
+
+	// Every java program requires a Main Method Signature
+	// This is the starting/entry point for every java program
+	// Follows the EXACT SAME syntax
+	
+	// Here is the main method signature
+	public static void main(String[] args){
+	
+	// public? Keyword that allows any and all class and programs to see this method
+	// static? defines at the class level and allows it to view this main method
+	// void? Return type, your main method is always void, as it should never return any value
+	
+	// main? is the method name, java specifically reads the file to find main
+	// String[]? An array of Strings, Strings a a string of characters. [] this defines an array
+	// args? This is the variable that is assigned to the added array of Strings
+	
+		System.out.println("Hello All you Wonderful People! You are absolutely killing it!"); // This prints out to console
+
+		// " " - String literal, it's exactly definining a String without a constructor
+		
+	}
+
+}
