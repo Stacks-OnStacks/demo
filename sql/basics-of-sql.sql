2eafd55a61826e145ce26cf37de58a30f482beba
JesterCharles <JesterCharles@gmail.com>
2022-08-04 09:54:20 -0400

    updated the sql information from yesterday

--- sql/basics-of-sql.sql
+++ sql/basics-of-sql.sql
@@ -60,7 +60,7 @@ on delete cascade;
 -- ctrl+enter runs whatever line your on
 insert into employee 
 values 
-('cj@mail.com', 'Charles Jester', '01-01-1900', 'Trainer', 1000, 'Technology', 2);
+('cj@mail.com', 'Charles', 'Jester', '01-01-1900', 'Trainer', 1000,  1);
 
 -- let's delte that user from the table
 delete from employee where email = 'ar@mail.com';
@@ -68,8 +68,8 @@ delete from employee where email = 'ar@mail.com';
 -- Multi-insert
 insert into employee
 values
-('ar@mail.com', 'Adam', 'Ranieri', '01-01-1000', 'Principle Trainer', 1005, 3),
-('hh@mail.com', 'Harvey', 'Hill', '01-01-100', 'Tech Manager', 10000, 3);
+('ar@mail.com', 'Adam', 'Ranieri', '01-01-1000', 'Principle Trainer', 1005, 1),
+('hh@mail.com', 'Harvey', 'Hill', '01-01-100', 'Tech Manager', 10000, 1);
 
 insert into employee
 values
@@ -153,17 +153,108 @@ create table citi.employee(
 ); 
 
 
+create schema p0;
+
+select * from employee;
+
+select * from department;
+
+
+-- read
+select * from employee;
+
+select first_name, dob, emp_position, email from employee; -- can be selective with select
+
+-- create a record
+insert into employee 
+(department_id, dob, emp_position, salary, last_name, first_name, email) -- should follow order of columns, can exclude anything you may not no so null is added by default
+values
+(2,  'Trainer', '01-01-0900', 1700, 'Arathyeal', 'Brian', 'bra@mail.coml');
+
+-- update 
+update employee set dob = '01-01-0900', emp_position = 'Trainer', email = 'bra@mail.com' where email = 'bra@mail.coml';
+
+-- delete
+delete from employee where email = 'ba@mail.coml';
+delete from employee where emp_position = 'Head of HR';
+
+select employee.email, employee.salary, employee.emp_position, department.department, department.phone_extension 
+from employee
+join department on employee.department_id = department.department_id;
+
+-- because of column specification order does not mater of which table is accessed first
+select employee.email, employee.salary, employee.emp_position, department.department, department.phone_extension 
+from department
+join employee on employee.department_id = department.department_id;
 
 
 
+select *
+from department
+join employee on employee.department_id = department.department_id;
 
 
+select f.email, f.first_name, f.salary, d.phone_extension, d.depart_floor, d.department 
+from employee f 
+join department d on f.department_id = d.department_id;
 
+create table enlistings(
+	training_name varchar(20),
+	department_id int,
+	constraint fk_department foreign key(department_id) references department(department_id)
+)
 
+drop table enlistings;
 
+insert into enlistings 
+values
+('Java React', 2);
 
+select *
+from employee e 
+join enlistings en  on e.department_id = en.department_id;
 
+-- Views
 
+create view employee_department as
+select f.email, f.first_name, f.salary, d.phone_extension, d.depart_floor, d.department 
+from employee f 
+join department d on f.department_id = d.department_id;
+
+insert into employee 
+values
+('sl@mail.com', 'Shelby', 'Lloyd', '01-01-2000', 'Support Specialist', 2000, );
+
+select * from employee
+union
+select * from walmart_employee;
+
+insert into employee 
+values
+('sw@mail.com', 'Shelby', 'Woods', '01-01-2000', 'HR Specialist', 4000, 2);
+
+-- Functions
+
+-- Scalar - they apply something to each record that's returning
+select upper(email) from employee; -- just changed in the query statement
+select lower(emp_position) from employee;
+select email, length(email) from employee e;
+
+select * from employee e ;
+
+-- Aggregate - taking multiple values and returning a single input
+
+-- before we do aggregate let's talk about grouping and order 
+
+select * from employee e order by salary; -- by default it orders ascendingly
+select * from employee e order by salary asc;
+select * from employee e order by salary desc limit 3;
+
+select avg(salary) from employee e; -- aggregate 
+
+select * from employee e limit 5;
+
+select department_id, avg(salary) from employee e group by department_id order by avg(salary) desc;
 
 
 
